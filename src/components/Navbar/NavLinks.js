7557c3990b4f6aaa08c693bf290432009d671cf5
Mauro Torres <maurotorresu@gmail.com>
2024-10-09 00:03:23 -0300

    modificación  03

--- src/components/Navbar/NavLinks.js
+++ src/components/Navbar/NavLinks.js
@@ -5,22 +5,24 @@ const NavLinks = () => {
     return (
         <>
             <HashLink className="px-4 font-extrabold text-gray-500 hover:text-blue-900" smooth to="/#about">
-                About
+                Nosotros
             </HashLink>
             <HashLink className="px-4 font-extrabold text-gray-500 hover:text-blue-900" smooth to="/#services">
-                Services
+                Servicios
             </HashLink>
             <HashLink className="px-4 font-extrabold text-gray-500 hover:text-blue-900" to="/">
-                Portfolio
+                Clientes
             </HashLink>
             <HashLink className="px-4 font-extrabold text-gray-500 hover:text-blue-900" to="/contact#contact">
-                Contact Us
+                Contáctanos
             </HashLink>
             <HashLink className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-auto px-6 py-3 shadow-xl rounded-xl" smooth to="/get-demo#demo">
-                Demo our products
+                Cotización
             </HashLink>
         </>
     )
 }
 
 export default NavLinks;
+
+
